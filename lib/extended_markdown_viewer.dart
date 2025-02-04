f41169f022c922a54fcbf66f9d2de78c017e195a
Rohan Kumar Panigrahi <rohan.panigrahi@raftlabs.co>
2025-02-05 01:24:49 +0530

    chore(package): Minor Code Changes - fixes sdk constraints

--- lib/extended_markdown_viewer.dart
+++ lib/extended_markdown_viewer.dart
@@ -1,3 +1,5 @@
+/// A Flutter package that displays markdown text in a flutter widget with support
+/// for simple html tags and expand/collapse functionality.
 library extended_markdown_viewer;
 
 export 'src/ex_markdown_viewer.dart';
